b75f1f055b7a530816e8f8252e75371bc87d90ed
Chung Kai Yu <spitwaters@gmail.com>
2021-09-23 20:43:09 +0800

    fix: change statement for scroller

--- src/Component/InfiniteScroll/InfiniteScroll.jsx
+++ src/Component/InfiniteScroll/InfiniteScroll.jsx
@@ -7,13 +7,11 @@ export const InfiniteScroll = (props) => {
 
   const handleScroll = () => {
     const scrollTop =
-      (document.documentElement && document.documentElement.scrollTop) ||
-      document.body.scrollTop;
+      document?.documentElement.scrollTop || document.body.scrollTop;
     const scrollHeight =
-      (document.documentElement && document.documentElement.scrollHeight) ||
-      document.body.scrollHeight;
+      document?.documentElement.scrollHeight || document.body.scrollHeight;
     const clientHeight =
-      document.documentElement.clientHeight || window.innerHeight;
+      document?.documentElement.clientHeight || window.innerHeight;
     const scrolledToBottom =
       Math.ceil(scrollTop + clientHeight) >= scrollHeight;
 
